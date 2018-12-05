a67417ea2483b9652998207355f3bd9b7573cf62
John Doe <johndoe@example.com>
2018-12-05 15:33:52 -0700

    Adds javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+var minInput = document.querySelector('.min-range-input');
+var maxInput = document.querySelector('.max-range-input');
+var updateButton = document.querySelector('#update-button');
+var currentMin = document.querySelector('.current-min');
+var currentMax = document.querySelector('.current-max');
+
