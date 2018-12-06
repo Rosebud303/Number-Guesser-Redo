dc04a404aae3aec3ba1a9d4ad5e049209349cadf
John Doe <johndoe@example.com>
2018-12-06 11:15:40 -0700

    Adds more js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-var minInput = document.querySelector('.min-range-input');
+var minInput = document.querySelector('#min-range-input');
 var maxInput = document.querySelector('.max-range-input');
 var currentMin = document.querySelector('.current-min');
 var currentMax = document.querySelector('.current-max');
 var updateButton = document.querySelector('#update-button');
+
 
 updateButton.addEventListener('click', updateRange);
 
@@ -10,8 +11,17 @@ updateButton.addEventListener('click', updateRange);
 
 function updateRange(event) {
   event.preventDefault();
-  currentMin.innerText = parseInt(minInput.value);
-  currentMax.innerText = parseInt(maxInput.value);
+  currentMin.innerText = minInput.value;
+  currentMax.innerText = maxInput.value;
+  var max = parseInt(maxInput.value);
+  var min = parseInt(minInput.value);
+  randomNumber(max, min);
   console.log(currentMax);
   console.log(currentMin);
 }
+
+function randomNumber(max, min) {
+  console.log(Math.floor(Math.random()*(max-min+1)+min));
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
